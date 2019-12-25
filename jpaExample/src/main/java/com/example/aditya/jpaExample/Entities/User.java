1998bf5630405bb3d9506fe9f91729d89d9c519b
aditya <aditya.rajiv@blackbuck.com>
2019-12-25 12:10:17 +0530

    project started

---
+++ jpaExample/src/main/java/com/example/aditya/jpaExample/Entities/User.java
@@ -0,0 +1,15 @@
+package com.example.aditya.jpaExample.Entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String branch;
+
+    private Double salary;
+}
